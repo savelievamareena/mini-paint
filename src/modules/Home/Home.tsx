bc2254bbea6fd6b5a870e8086a6dc0ea67037771
savelievamareena <savelieva.mareena.1.21@gmail.com>
2024-05-20 19:16:05 +0300

    feat: add pagination for HomePage

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -1,65 +1,85 @@
-import { useEffect, useState } from "react";
-import { collection, DocumentData, getDocs, query, where, orderBy } from "firebase/firestore";
-import { db } from "firebase";
+import { ChangeEvent, useEffect, useState } from "react";
+import { collection, DocumentData, getDocs, orderBy, query, where } from "firebase/firestore";
 import { useAuth } from "../../context/AuthContext";
 import getPicsFromDb from "./helpers/getPicsFromDb";
-import { retrieveEmailsFromPicData } from "./helpers/retrieveEmailsFromPicData";
 import { useModalHandlers, usePictureDelete } from "../Home/hooks";
 import { PictureCard } from "./components/PictureCard";
 import handleDbErrors from "src/helpers/handleDbError";
 import { cardElementsWrapper, cardsContainer, select, modal } from "./Home.styles";
-import { MenuItem, Modal, Select, SelectChangeEvent, Container, Box } from "@mui/material";
+import {
+    MenuItem,
+    Modal,
+    Select,
+    SelectChangeEvent,
+    Container,
+    Box,
+    Pagination,
+} from "@mui/material";
+import useCollectionRef from "./hooks/useCollectionRef";
+import { db } from "firebase";
+import { retrieveEmailsFromPicData } from "./helpers/retrieveEmailsFromPicData";
 
 const Home = () => {
     const { currentUser } = useAuth();
+    const numberOfItemsPerPage = 4;
 
     const [picturesData, setPicturesData] = useState<DocumentData[]>([]);
     const [usersEmails, setUsersEmails] = useState<string[]>([]);
-    const [selectedEmailId, setSelectedEmailId] = useState("-1");
+    const [selectedEmail, setSelectedEmail] = useState("-1");
+    const [pagesNumber, setPagesNumber] = useState(1);
+    const [currentPage, setCurrentPage] = useState(1);
 
     const { handleModalOpen, handleModalClose, picToShow, modalOpen } = useModalHandlers();
+    const getCollectionRef = useCollectionRef(numberOfItemsPerPage, currentPage, selectedEmail);
     const { handleDelete } = usePictureDelete(setPicturesData);
 
     useEffect(() => {
         (async () => {
-            const collectionRef = query(collection(db, "pics"), orderBy("createdAt", "desc"));
-            try {
-                const snapshot = await getDocs(collectionRef);
+            let baseQuery = query(collection(db, "pics"), orderBy("createdAt", "desc"));
+            let snapshot;
 
-                const docs = getPicsFromDb(snapshot);
-                setPicturesData(docs);
-                setUsersEmails(retrieveEmailsFromPicData(docs));
+            if (selectedEmail === "-1") {
+                snapshot = await getDocs(baseQuery);
+                const totalDocuments = snapshot.size;
+                setPagesNumber(Math.ceil(totalDocuments / numberOfItemsPerPage));
+
+                const allPicData = getPicsFromDb(snapshot);
+                setUsersEmails(retrieveEmailsFromPicData(allPicData));
+            } else {
+                baseQuery = query(baseQuery, where("userEmail", "==", selectedEmail));
+                snapshot = await getDocs(baseQuery);
+                const totalDocuments = snapshot.size;
+                setPagesNumber(Math.ceil(totalDocuments / numberOfItemsPerPage));
+            }
+        })();
+    }, [selectedEmail, numberOfItemsPerPage]);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const collectionRef = await getCollectionRef();
+                const snapshot = await getDocs(collectionRef);
+                const data = getPicsFromDb(snapshot);
+                setPicturesData(data);
             } catch (error: unknown) {
                 handleDbErrors(error);
             }
         })();
-    }, []);
+    }, [currentPage, selectedEmail]);
 
     async function handleSelectChange(event: SelectChangeEvent<string>) {
-        setSelectedEmailId(event.target.value);
-
-        const collectionRef = query(collection(db, "pics"), orderBy("createdAt", "desc"));
-        let queryRef;
-
-        if (event.target.value === "-1") {
-            queryRef = collectionRef;
-        } else {
-            const email = event.target.value;
-            queryRef = query(collectionRef, where("userEmail", "==", email));
-        }
-        try {
-            const snapshot = await getDocs(queryRef);
-
-            const docs = getPicsFromDb(snapshot);
-            setPicturesData(docs);
-        } catch (error: unknown) {
-            handleDbErrors(error);
-        }
+        setCurrentPage(1);
+        setSelectedEmail(event.target.value);
     }
 
-    const cardElements = picturesData.map(({ data, id }) => {
+    function handlePaginationChange(_event: ChangeEvent<unknown>, page: number) {
+        setCurrentPage(page);
+    }
+
+    const pictureCardElements = picturesData.map(({ data, id }) => {
         return (
             <PictureCard
+                key={id}
                 data={data}
                 id={id}
                 currentUser={currentUser}
@@ -71,7 +91,7 @@ const Home = () => {
 
     return (
         <Container sx={cardsContainer}>
-            <Select sx={select} value={selectedEmailId} onChange={handleSelectChange}>
+            <Select sx={select} value={selectedEmail} onChange={handleSelectChange}>
                 <MenuItem value={"-1"}>All users</MenuItem>
                 {usersEmails.map((userEmail, i) => {
                     return (
@@ -81,15 +101,17 @@ const Home = () => {
                     );
                 })}
             </Select>
-            <Box sx={cardElementsWrapper}>{cardElements}</Box>
+            <Box sx={cardElementsWrapper}>{pictureCardElements}</Box>
+            <Pagination
+                count={pagesNumber}
+                page={currentPage}
+                color='primary'
+                onChange={handlePaginationChange}
+                sx={{ marginBottom: "10px" }}
+            />
 
             <Modal open={modalOpen} onClose={handleModalClose}>
-                <Box
-                    component='img'
-                    sx={modal}
-                    alt={usersEmails[Number(selectedEmailId)]}
-                    src={picToShow}
-                />
+                <Box component='img' sx={modal} alt={selectedEmail} src={picToShow} />
             </Modal>
         </Container>
     );
